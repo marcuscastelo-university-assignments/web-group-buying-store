df3127b2cadbbed722e8b015e81c5d3c5dae264b
marcuscastelo <marcuscastelo@usp.br>
2021-05-25 17:00:44 -0300

    Adding milestones first logic

---
+++ frontend/js/buypage.js
@@ -0,0 +1,245 @@
+
+let carrouselSalesInfo = [
+    {
+        "itemsPerPage": 5,
+        "title": "Interesses",
+        "items": [
+            { "curQtty": 3, "milestones": [{ "quantity": 3, "price": 25 }, { "quantity": 6, "price": 15 }], "alt": "item0", "id": "aid1c0", "link": "url", "imageSrc": "https://picsum.photos/200" },
+            { "curQtty": 4, "milestones": [{ "quantity": 3, "price": 25 }, { "quantity": 5, "price": 15 }], "alt": "item1", "id": "aid1c1", "link": "url", "imageSrc": "https://picsum.photos/300" },
+            { "curQtty": 4, "milestones": [{ "quantity": 3, "price": 25 }, { "quantity": 5, "price": 15 }], "alt": "item2", "id": "aid1c2", "link": "url", "imageSrc": "https://picsum.photos/400" },
+            { "curQtty": 4, "milestones": [{ "quantity": 3, "price": 25 }, { "quantity": 5, "price": 15 }], "alt": "item3", "id": "aid1c3", "link": "url", "imageSrc": "https://picsum.photos/500" },
+            { "curQtty": 4, "milestones": [{ "quantity": 3, "price": 25 }, { "quantity": 5, "price": 15 }], "alt": "item4", "id": "aid1c4", "link": "url", "imageSrc": "https://picsum.photos/600" },
+            { "curQtty": 4, "milestones": [{ "quantity": 3, "price": 25 }, { "quantity": 5, "price": 15 }], "alt": "item5", "id": "aid1c5", "link": "url", "imageSrc": "https://picsum.photos/700" },
+            { "curQtty": 4, "milestones": [{ "quantity": 3, "price": 25 }, { "quantity": 5, "price": 15 }], "alt": "item6", "id": "aid1c6", "link": "url", "imageSrc": "https://picsum.photos/800" },
+            { "curQtty": 4, "milestones": [{ "quantity": 3, "price": 25 }, { "quantity": 5, "price": 15 }], "alt": "item7", "id": "aid1c7", "link": "url", "imageSrc": "https://picsum.photos/900" },
+            { "curQtty": 4, "milestones": [{ "quantity": 3, "price": 25 }, { "quantity": 5, "price": 15 }], "alt": "item8", "id": "aid1c8", "link": "url", "imageSrc": "https://picsum.photos/1000" },
+            { "curQtty": 4, "milestones": [{ "quantity": 3, "price": 25 }, { "quantity": 5, "price": 15 }], "alt": "item9", "id": "aid1c9", "link": "url", "imageSrc": "https://picsum.photos/1100" }
+
+        ]
+    },
+
+    {
+        "itemsPerPage": 2,
+        "title": "Para você",
+        "items": [
+            { "curQtty": 4, "milestones": [{ "quantity": 3, "price": 25 }, { "quantity": 5, "price": 15 }], "alt": "item0", "id": "aid2c0", "link": "url", "imageSrc": "https://picsum.photos/200" },
+            { "curQtty": 2, "milestones": [{ "quantity": 3, "price": 25 }, { "quantity": 5, "price": 15 }], "alt": "item1", "id": "aid2c1", "link": "url", "imageSrc": "https://picsum.photos/200" },
+            { "curQtty": 1, "milestones": [{ "quantity": 3, "price": 25 }, { "quantity": 5, "price": 15 }], "alt": "item2", "id": "aid2c2", "link": "url", "imageSrc": "https://picsum.photos/200" },
+            { "curQtty": 9, "milestones": [{ "quantity": 3, "price": 25 }, { "quantity": 5, "price": 15 }], "alt": "item3", "id": "aid2c3", "link": "url", "imageSrc": "https://picsum.photos/200" },
+            { "curQtty": 7, "milestones": [{ "quantity": 3, "price": 25 }, { "quantity": 5, "price": 15 }], "alt": "item4", "id": "aid2c4", "link": "url", "imageSrc": "https://picsum.photos/200" },
+            { "curQtty": 5, "milestones": [{ "quantity": 3, "price": 25 }, { "quantity": 5, "price": 15 }], "alt": "item5", "id": "aid2c5", "link": "url", "imageSrc": "https://picsum.photos/200" },
+            { "curQtty": 3, "milestones": [{ "quantity": 3, "price": 25 }, { "quantity": 5, "price": 15 }], "alt": "item6", "id": "aid2c6", "link": "url", "imageSrc": "https://picsum.photos/200" },
+            { "curQtty": 2, "milestones": [{ "quantity": 3, "price": 25 }, { "quantity": 5, "price": 15 }], "alt": "item7", "id": "aid2c7", "link": "url", "imageSrc": "https://picsum.photos/200" },
+            { "curQtty": 5, "milestones": [{ "quantity": 3, "price": 25 }, { "quantity": 5, "price": 15 }], "alt": "item8", "id": "aid2c8", "link": "url", "imageSrc": "https://picsum.photos/200" },
+            { "curQtty": 0, "milestones": [{ "quantity": 3, "price": 25 }, { "quantity": 5, "price": 15 }], "alt": "item9", "id": "aid2c9", "link": "url", "imageSrc": "https://picsum.photos/200" }
+
+        ]
+    },
+    {
+        "itemsPerPage": 6,
+        "title": "Próximos de esgotar",
+        "items": [
+            { "curQtty": 4, "milestones": [{ "quantity": 30, "price": 25 }, { "quantity": 500, "price": 15 }], "alt": "item0", "id": "aid3c0", "link": "url", "imageSrc": "https://picsum.photos/200" },
+            { "curQtty": 2, "milestones": [{ "quantity": 3, "price": 25 }, { "quantity": 5, "price": 15 }], "alt": "item1", "id": "aid3c1", "link": "url", "imageSrc": "https://picsum.photos/200" },
+            { "curQtty": 1, "milestones": [{ "quantity": 3, "price": 25 }, { "quantity": 5, "price": 15 }], "alt": "item2", "id": "aid3c2", "link": "url", "imageSrc": "https://picsum.photos/200" },
+            { "curQtty": 9, "milestones": [{ "quantity": 3, "price": 25 }, { "quantity": 5, "price": 15 }], "alt": "item3", "id": "aid3c3", "link": "url", "imageSrc": "https://picsum.photos/200" },
+            { "curQtty": 7, "milestones": [{ "quantity": 3, "price": 25 }, { "quantity": 5, "price": 15 }], "alt": "item4", "id": "aid3c4", "link": "url", "imageSrc": "https://picsum.photos/200" },
+            { "curQtty": 5, "milestones": [{ "quantity": 3, "price": 25 }, { "quantity": 5, "price": 15 }], "alt": "item5", "id": "aid3c5", "link": "url", "imageSrc": "https://picsum.photos/200" },
+            { "curQtty": 3, "milestones": [{ "quantity": 3, "price": 25 }, { "quantity": 5, "price": 15 }], "alt": "item6", "id": "aid3c6", "link": "url", "imageSrc": "https://picsum.photos/200" },
+            { "curQtty": 2, "milestones": [{ "quantity": 3, "price": 25 }, { "quantity": 5, "price": 15 }], "alt": "item7", "id": "aid3c7", "link": "url", "imageSrc": "https://picsum.photos/200" },
+            { "curQtty": 5, "milestones": [{ "quantity": 3, "price": 25 }, { "quantity": 5, "price": 15 }], "alt": "item8", "id": "aid3c8", "link": "url", "imageSrc": "https://picsum.photos/200" },
+            { "curQtty": 0, "milestones": [{ "quantity": 3, "price": 25 }, { "quantity": 5, "price": 15 }], "alt": "item9", "id": "aid3c9", "link": "url", "imageSrc": "https://picsum.photos/200" }
+
+        ]
+    },
+    {
+        "itemsPerPage": 1,
+        "title": "Em promoção",
+        "items": [
+            { "curQtty": 4, "milestones": [{ "quantity": 3, "price": 25 }, { "quantity": 5, "price": 15 }], "alt": "item0", "id": "aid4c0", "link": "product?id=id4c0", "imageSrc": "https://picsum.photos/200" },
+            { "curQtty": 2, "milestones": [{ "quantity": 3, "price": 25 }, { "quantity": 5, "price": 15 }], "alt": "item1", "id": "aid4c1", "link": "product?id=id4c1", "imageSrc": "https://picsum.photos/200" },
+            { "curQtty": 1, "milestones": [{ "quantity": 3, "price": 25 }, { "quantity": 5, "price": 15 }], "alt": "item2", "id": "aid4c2", "link": "product?id=id4c2", "imageSrc": "https://picsum.photos/200" },
+            { "curQtty": 9, "milestones": [{ "quantity": 3, "price": 25 }, { "quantity": 5, "price": 15 }], "alt": "item3", "id": "aid4c3", "link": "product?id=id4c3", "imageSrc": "https://picsum.photos/200" },
+            { "curQtty": 7, "milestones": [{ "quantity": 3, "price": 25 }, { "quantity": 5, "price": 15 }], "alt": "item4", "id": "aid4c4", "link": "product?id=id4c4", "imageSrc": "https://picsum.photos/200" },
+            { "curQtty": 5, "milestones": [{ "quantity": 3, "price": 25 }, { "quantity": 5, "price": 15 }], "alt": "item5", "id": "aid4c5", "link": "product?id=id4c5", "imageSrc": "https://picsum.photos/200" },
+            { "curQtty": 3, "milestones": [{ "quantity": 3, "price": 25 }, { "quantity": 5, "price": 15 }], "alt": "item6", "id": "aid4c6", "link": "product?id=id4c6", "imageSrc": "https://picsum.photos/200" },
+            { "curQtty": 2, "milestones": [{ "quantity": 3, "price": 25 }, { "quantity": 5, "price": 15 }], "alt": "item7", "id": "aid4c7", "link": "product?id=id4c7", "imageSrc": "https://picsum.photos/200" },
+            { "curQtty": 5, "milestones": [{ "quantity": 3, "price": 25 }, { "quantity": 5, "price": 15 }], "alt": "item8", "id": "aid4c8", "link": "product?id=id4c8", "imageSrc": "https://picsum.photos/200" },
+            { "curQtty": 0, "milestones": [{ "quantity": 3, "price": 25 }, { "quantity": 5, "price": 15 }], "alt": "item9", "id": "aid4c9", "link": "product?id=id4c9", "imageSrc": "https://picsum.photos/200" }
+
+        ]
+    },
+];
+/*
+function getItemAttrs(item) {
+    let minPriceMile = { price: item.milestones[0].price + 1 };
+    let curPrice;
+    let curQtty = item.curQtty;
+    let nearestMile = { "quantity": -1 };
+    let biggestMile = { "quantity": -1 };
+    for (milestone of item.milestones) {
+        if (milestone.quantity > biggestMile.quantity)
+            biggestMile.quantity = milestone.quantity;
+
+        if (curQtty > milestone.quantity && milestone.quantity > nearestMile.quantity)
+            nearestMile = milestone;
+
+        if (milestone.price < minPriceMile.price) minPriceMile = milestone;
+    }
+
+    if (biggestMile.quantity === -1) {
+        console.error('Invalid product: no valid milestones. ID = ', item.id);
+    }
+
+    if (nearestMile.quantity === -1) curPrice = '-';
+    else curPrice = nearestMile.price;
+
+    return { curQtty, maxQuantity: biggestMile.quantity, curPrice, minPrice: minPriceMile.price };
+}
+
+function generateItem(item) {
+    let itemAttrs = getItemAttrs(item);
+    return `
+        <div class="col mx-auto" style="min-width:00px; max-width:200px">
+            <div class="card">
+                <a href=${item.link}>
+                    <img src="${item.imageSrc}" class="d-block img-thumbnail mx-auto" alt="${item.alt}" />
+                </a>
+                <div class="card-body px-1 pt-2">
+                    <div class="row g-0 p-0">
+                        <div class="col-10" style="text-transform:capitalize;"> ${item.alt} </div>
+                    </div>
+                    <div class="row flex-direction-row mt-2 px-1">
+                        <div class="col-6">
+                            R$${itemAttrs.curPrice}
+                        </div>
+                        <div class="col-6 text-end">
+                            R$${itemAttrs.minPrice}
+                        </div>
+                    </div>
+                    <div class="progress">
+                        <div class="progress-bar text-center bg-warning" role="progressbar" style="width: ${100 * itemAttrs.curQtty / itemAttrs.maxQuantity}%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100">
+                        <small class="justify-content-center d-flex position-absolute w-100 text-dark fw-bold">${itemAttrs.curQtty}/${itemAttrs.maxQuantity}</small>
+                        
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `
+}
+*/
+
+$(document).ready(function () {
+    /*
+    async function populateCarrousel(carrouselType, location, idName, generateItem, classes) {
+        classes = classes || '';
+
+        let itemsPerPage = Math.min(carrouselType.itemsPerPage, carrouselType.items.length);
+        if (itemsPerPage <= 0) return;
+
+        let newHtml = `<div class="row layer ${classes} carousel-container mt-2" id="carousel-${idName}-row">
+                        <h4 class="${classes || 'mt-5'} w-100 text-center ${classes}  ">${carrouselType.title}</h4>
+                        <div class="col-9 mx-auto w-100">
+                            <div class="carousel slide" id="carousel-${idName}" data-bs-ride="carousel">
+                                <div class="carousel-inner w-75 mx-auto">`;
+
+
+        let pageCount = Math.ceil(carrouselType.items.length / itemsPerPage);
+        let lastPageItemsToAdd = (itemsPerPage - carrouselType.items.length % itemsPerPage) % itemsPerPage;
+        for (let i = 0; i < pageCount; i++) {
+            newHtml += `<div class="${'carousel-item row-car ' + (i === 0 ? 'active' : '')} ">
+                            <div class="row g-1">`
+            let k = i * itemsPerPage;
+
+            if (i === pageCount - 1) k -= lastPageItemsToAdd;
+
+            for (let j = 0; j < itemsPerPage; j++) {
+                let item = carrouselType.items[k + j];
+                newHtml += generateItem(item);
+            }
+            newHtml += `
+                            </div>
+                        </div>
+            `;
+        }
+        newHtml += "</div>";
+        newHtml += `
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carousel-${idName}" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carousel-${idName}" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+        `
+
+        newHtml += `
+                        </div>
+                    </div>
+                </div>
+            `;
+
+        let currHtml = $(location).html();
+        $(location).html(currHtml + newHtml);
+
+    }
+    */
+    function populateMilestones(milestoneInfo) {
+        let newHtml = ''
+
+        for(let milestone in milestoneInfo){
+            console.log("foi mais um");
+            newHtml += `
+
+            <div class="milestone-item row card mx-auto m-2 p-1 w-100" data-milestone="${'foda'}">
+                <div class="card-header">
+                    a
+                </div>
+                <div class="card-body d-none">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text d-none">With supporting text below as a natural lead-in to additional content.</p>
+                </div>
+            </div>
+            `  
+        }
+
+
+        let currHtml = $('#milestone-list').html();
+        $('#milestoneList').html(currHtml + newHtml);
+    }
+
+    populateMilestones(carrouselSalesInfo[0].milestones);
+    //Mouse Events
+    $('.milestone-item').click(function (e) {
+        e.preventDefault();
+        console.log('asas')
+
+        //Hides every other milestone in the list
+        let self = $(this);
+        
+        let isActive = self.hasClass('active');
+        
+        let targets = $('#milestone-list .milestone-item')
+        targets.addClass('d-none');
+        self.removeClass('d-none');
+        
+        targets.find('.card-body').addClass('d-none');
+        self.find('.card-body').removeClass('d-none');
+
+    });
+    /*
+
+    $('#categories').mouseleave(function (e) {
+        e.preventDefault();
+
+        let layers = $(this).find(".layer[data-layer]:not([data-layer='1']")
+        layers.addClass('d-none');
+        layers.find('.category').addClass('d-none');
+        $('#carousel-layer-row').addClass('d-none');
+
+
+    });
+    */
+});
--- frontend/js/cart.js
+++ frontend/js/cart.js
@@ -11,13 +11,21 @@ $(document).ready(function () {
     });
 
     function updateQuantity(caller, delta) {
-        let cardParent = $(caller).parent().parent().parent();
+        let cardParent = $(caller).parent().parent().parent().parent().parent();
         let quantityField = cardParent.find('.quantity');
         let newQuant = Math.max(1, parseInt(quantityField.val()) + delta);
         quantityField.val(newQuant);
         cardParent.find('span.quantity .value').html(newQuant);
-        let price = parseInt(cardParent.find('span.cart-item-price .value').html())
-        cardParent.find('.cart-item-total .value').html(newQuant * price)
+        let price = parseInt(cardParent.find('span.cart-item-price .value').html());
+        cardParent.find('.cart-item-total .value').html(newQuant * price);
+
+        updateTotal();
+    }
+
+    function updateTotal() {
+        let total = 0;
+        $('.cart-item-total .value').each(function (_) { total += parseInt($(this).html()) });
+        $('.cart-total .value').html(total);
     }
 
 });
